882fe7a2ea0fd3654342d989de494ab76b538e28
Gauravjot Garaya <gauravjot@outlook.com>
2024-07-07 12:41:27 -0700

    added date field

--- src/screens/Dashboard/NewAppointmentTab.tsx
+++ src/screens/Dashboard/NewAppointmentTab.tsx
@@ -9,12 +9,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@src/components/ui/select';
-import {useForm} from 'react-hook-form';
-import {ScrollView, View, useColorScheme} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
+import {
+  Platform,
+  Pressable,
+  ScrollView,
+  View,
+  useColorScheme,
+} from 'react-native';
 import {Text} from '@src/components/ui/text';
 import {getTwColors} from '@src/styles/styles';
 import Animated, {FadeInDown} from 'react-native-reanimated';
 import {Button} from '@src/components/ui/button';
+import {useState} from 'react';
+import {Calendar} from 'react-native-calendars';
+import {datePretty} from '@src/utils/timeUtils';
+import {Label} from '@src/components/ui/label';
 
 export default function NewAppointmentTab({navigation}: {navigation: any}) {
   const theme = useColorScheme() || 'light';
@@ -31,6 +41,8 @@ export default function NewAppointmentTab({navigation}: {navigation: any}) {
     handleSubmit,
     formState: {errors},
   } = useForm();
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+  const [show, setShow] = useState(false);
 
   return (
     <Animated.View entering={FadeInDown.duration(200)} className="min-h-screen">
@@ -58,46 +70,92 @@ export default function NewAppointmentTab({navigation}: {navigation: any}) {
             </Text>
           </View>
           <View className="px-4 py-2">
+            <Controller
+              control={control}
+              rules={{required: 'Type is required'}}
+              name="date"
+              render={({field: {onChange, onBlur, value}}) => (
+                <View className="flex gap-1.5">
+                  <Label className="px-2" nativeID="date">
+                    Date
+                  </Label>
+                  <Button
+                    variant={'outline'}
+                    className="items-start justify-start"
+                    onPress={() => {
+                      setShow(v => !v);
+                    }}>
+                    <Text className="-mt-0.5">{value}</Text>
+                  </Button>
+                  {show && (
+                    <View className="border rounded-lg border-muted-foreground/20">
+                      <Calendar
+                        onDayPress={day => {
+                          onChange(
+                            new Date(day.dateString)
+                              .toISOString()
+                              .split('T')[0],
+                          );
+                          setShow(false);
+                        }}
+                      />
+                    </View>
+                  )}
+                </View>
+              )}
+            />
             <FormInput
               control={control}
               label="Name"
               name="name"
               rules={{required: 'Name is required'}}
             />
-            <Select defaultValue={{value: 'apple', label: 'Apple'}}>
-              <SelectTrigger className="w-[250px]">
-                <SelectValue
-                  className="text-sm text-foreground native:text-lg"
-                  placeholder="Select a fruit"
-                />
-              </SelectTrigger>
-              <SelectContent insets={contentInsets} className="w-[250px]">
-                <SelectGroup>
-                  <SelectLabel>Fruits</SelectLabel>
-                  <SelectItem label="Apple" value="apple">
-                    Apple
-                  </SelectItem>
-                  <SelectItem label="Banana" value="banana">
-                    Banana
-                  </SelectItem>
-                  <SelectItem label="Blueberry" value="blueberry">
-                    Blueberry
-                  </SelectItem>
-                  <SelectItem label="Grapes" value="grapes">
-                    Grapes
-                  </SelectItem>
-                  <SelectItem label="Pineapple" value="pineapple">
-                    Pineapple
-                  </SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            {errors.name && <Text>Some error</Text>}
+            {errors.type && <Text>Some error type</Text>}
+            {errors.date && <Text>Some error date</Text>}
+            <Controller
+              control={control}
+              rules={{required: 'Type is required'}}
+              render={({field: {onChange, onBlur, value}}) => (
+                <Select
+                  value={{value: value, label: 'Voice'}}
+                  onValueChange={value => {
+                    console.log(value);
+                    onChange(value);
+                  }}>
+                  <SelectTrigger className="w-[250px]">
+                    <SelectValue
+                      className="text-sm text-foreground native:text-lg"
+                      placeholder="Select type"
+                    />
+                  </SelectTrigger>
+                  <SelectContent insets={contentInsets} className="w-[250px]">
+                    <SelectGroup>
+                      <SelectLabel>Types</SelectLabel>
+                      <SelectItem label="Voice" value="voice">
+                        Voice
+                      </SelectItem>
+                      <SelectItem label="Video" value="video">
+                        Video
+                      </SelectItem>
+                      <SelectItem label="In-person" value="in-person">
+                        In-person
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              )}
+              name="type"
+            />
           </View>
         </ScrollView>
         <Button
           variant={'default'}
           size="lg"
-          className="rounded-t-none rounded-b-lg bg-accent-foreground">
+          className="rounded-t-none rounded-b-lg bg-accent-foreground"
+          onPress={handleSubmit(data => {
+            console.log(data);
+          })}>
           <Text>Submit</Text>
         </Button>
       </View>
